d31fee4499b6167651b3b1504102af099aa90bbb
jandersonvb <jandersonvb.dev@gmail.com>
2024-09-26 23:35:44 -0300

    chore: configure husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+module.exports = {
+  // Lint & Prettify TS and JS files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `npm run lint --fix . ${filenames.join(' --file')}`
+  ]
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  const foo = 'bar'
-
   return <h1>Hello World</h1>
 }
